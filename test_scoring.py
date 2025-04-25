68bd50fe3a1711c6da4b06b5345f1d516ba0c004
Johnsa04 <anthony.johns2005@gmail.com>
2025-04-25 23:47:05 +0100

    added test for scoring - anthony

---
+++ test_scoring.py
@@ -0,0 +1,20 @@
+from scoring import calculate_smurf_score, classify_account
+
+#test calculate_smurf_score function
+def test_calculate_smurf_score(): 
+    user_info = {"account_age" : 300}
+    friend_count = 20
+    games_owned = 7
+    playtime = 5000
+    vac_ban = 0
+    expected_score = 4
+
+    assert calculate_smurf_score(user_info, friend_count, games_owned, playtime, vac_ban) == expected_score
+
+#test classify_account function
+def test_classify_account():
+    assert classify_account(2) == "Likely Genuine"
+    assert classify_account(10) == "Likely Smurf"
+    
+
+
